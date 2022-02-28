478c1d1ed3ef6db9505dcf8b0fb48c414c384824
LaptevaKate <ekaterina213@yandex.ru>
2022-02-28 12:57:15 +0300

    first commit

---
+++ HomeWork7-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork7-2
+//
+//  Created by Екатерина Лаптева on 28.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
